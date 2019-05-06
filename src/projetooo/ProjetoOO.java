e0f317c909e2404a9968ff91ecbd066ecc6b77f3
Aluno <aluno@ldc.local>
2019-05-06 20:07:44 -0300

    Initial Commit

---
+++ src/projetooo/ProjetoOO.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetooo;
+
+import java.awt.Color;
+
+
+/**
+ *
+ * @author Hybriun System
+ */
+public class ProjetoOO {
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Login login = new Login();
+        login.setVisible(true);
+    }
+
+}
